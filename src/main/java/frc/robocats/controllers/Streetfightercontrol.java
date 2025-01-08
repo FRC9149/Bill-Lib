88447e7e4e4897441847ea8d41a599bbe5a68180
RoboEthan1 <eplank.2028@students.rfsd.k12.wi.us>
2025-01-08 15:32:21 -0600

    start of streetfighter

---
+++ src/main/java/frc/robocats/controllers/Streetfightercontrol.java
@@ -0,0 +1,13 @@
+public class Streetfightercontrol implements Controller {
+    Joystick Button = new Joystick(0);
+  /** Do not use, has not been implemented. Requires implementor to Override the JavaDoc */
+  public Trigger on1(){
+    
+  }
+  /** Do not use, has not been implemented. Requires implementor to Override the JavaDoc */
+  public Trigger on2();
+  /** Do not use, has not been implemented. Requires implementor to Override the JavaDoc */
+  public Trigger on3();
+  /** Do not use, has not been implemented. Requires implementor to Override the JavaDoc */
+  public Trigger on4();    
+}
