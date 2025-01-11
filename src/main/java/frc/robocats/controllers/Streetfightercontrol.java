362b0be113042238f8e3f32d02d945bb8831b2b1
= <=>
2025-01-10 21:43:03 -0600

    removed errors from streetfighter

--- src/main/java/frc/robocats/controllers/Streetfightercontrol.java
+++ src/main/java/frc/robocats/controllers/Streetfightercontrol.java
@@ -1,13 +1,2 @@
-public class Streetfightercontrol implements Controller {
-    Joystick Button = new Joystick(0);
-  /** Do not use, has not been implemented. Requires implementor to Override the JavaDoc */
-  public Trigger on1(){
-    
-  }
-  /** Do not use, has not been implemented. Requires implementor to Override the JavaDoc */
-  public Trigger on2();
-  /** Do not use, has not been implemented. Requires implementor to Override the JavaDoc */
-  public Trigger on3();
-  /** Do not use, has not been implemented. Requires implementor to Override the JavaDoc */
-  public Trigger on4();    
+public class Streetfightercontrol /*implements Controller*/ {  
 }
