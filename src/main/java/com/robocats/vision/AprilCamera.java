690df8cf67f703466f6fec17c62e2af7affdf830
El <ecampus2026@gmail.com>
2026-01-23 01:16:01 -0600

    Researched Limelight pose Esitimator and prepared for that addition. Updated camera to be an interface and started writing those functions. "removed" the old AprilCamera.java

--- src/main/java/com/robocats/vision/AprilCamera.java
+++ src/main/java/com/robocats/vision/AprilCamera.java
@@ -1,97 +1,114 @@
 package com.robocats.vision;
 
-import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
-import org.photonvision.targeting.PhotonTrackedTarget;
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose3d;
+import org.photonvision.struct.PhotonTrackedTargetSerde;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
 
-public class AprilCamera extends Camera {
-  AprilTagFieldLayout tagLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeAndyMark);
-  PhotonPoseEstimator estimator = new PhotonPoseEstimator(tagLayout, PhotonPoseEstimator.PoseStrategy.AVERAGE_BEST_TARGETS, super.transform );
-
-  public AprilCamera(String cameraBroadcastingName, Transform3d cameraTransform) {
-    super(cameraBroadcastingName, cameraTransform);
-  }
-
+/** A interface for creating a camera that interprets April Tags.
+ * @author El Campus
+ * Times rewrote because I didn't think it through: 3
+ */
+public interface AprilCamera {
+  public double getDistanceMeters();
+  public Pose2d getRobotPose();
   /**
-   * @param id The id of the tag you want to align to (-1 for best target)
-   * @param pid The pid controller to control the movement. leave null for default
-   * @return The directional speed that you should turn to get to the target
+   * @return a ChassisSpeeds that describe the motion required to face the target
    */
-  public double alignWithTag(int id, PIDController pid) {
-    double yaw = super.getYaw();
-
-    if (pid == null) {
-      pid = new PIDController(0.039, 0.059, 0);
-    }
-    return pid.calculate(yaw, 0)/1.5;
-  }
-
-  public double squareWithTag(int id, PIDController pid, double robotRotationRadians) {
-    double zAngle = isResultUsable() ? 
-    getTargetPose(getAprilTagId()).getRotation().getZ() : 0;
-
-    if (pid == null) {
-      pid = new PIDController(0.3, 0, 0);
-    }
-    return pid.calculate(robotRotationRadians, zAngle);
-  }
-  public double getZAngleRadians() {
-    return isResultUsable() ? result.getBestTarget().getBestCameraToTarget().getRotation().getAngle() : 0;
-  }
-
-  public double tagHeight(int id) {
-    if (tagLayout == null) { return -1; }
-    return tagLayout.getTagPose(id).get().getTranslation().getZ();
-  }
-
-  /** @return The id of the best target. -1 if there is none */
-  public int getAprilTagId() {
-    return isResultUsable() ? result.getBestTarget().fiducialId : -1;
-  }
-
-  public Pose3d getRobotPose() {
-    if (!isResultUsable()) return null;
-    var result = estimator.update(super.result);
-    return result.isPresent() ? result.get().estimatedPose : null;
-  }
-  public Pose3d getTargetPose(int id) {
-    return tagLayout.getTagPose(id).orElse(null);
-  }
-
-  /**Distance Left and right (Left positive)*/
-  public double getDistanceY() {
-    return isResultUsable() ? super.result.getBestTarget().getBestCameraToTarget().getY() : -1;
-    // Pose3d robotPose = getRobotPose();
-    // if (robotPose == null || !isResultUsable()) { return -1; }
-    // return PhotonUtils.getDistanceToPose(robotPose.toPose2d(), getTargetPose(getAprilTagId()).toPose2d());
-  }
+  public ChassisSpeeds faceTag();
   /**
-   * Distance Forward and Back (Backwards positive)
-   * @return
+   * Saves and image of what the camera currently sees
    */
-  public double getDistanceX() {
-    return isResultUsable() ? super.result.getBestTarget().getBestCameraToTarget().getX() : -1;
-  }
-
-  /**
-   * Used to make sure that you are looking for a certain tag
-   * 
-   * @param id The id of the April tag you're looking for
-   * @return The Tracked Target of a april tag with a certain Id; null if these
-   *         are not the tags you are looking for
-   *         {@link https://www.youtube.com/watch?v=532j-186xEQ}
-   */
-  public PhotonTrackedTarget getTargetWithId(int id) {
-    if (!isResultUsable()) return null;
-
-    for (PhotonTrackedTarget target : result.getTargets()) {
-      if (target.fiducialId == id) return target;
-    }
-    return null;
-  }
+  public void screenshot();
 }
+
+
+
+
+// public class Camera {
+//   protected PhotonCamera camera;
+//   protected Transform3d transform;
+//   protected PhotonPipelineResult result;
+
+//   /**
+//    * @param cameraBroadcastingName The name of the camera (found in Photon client settings top right)
+//    * @param camreaHeightMeters     how high the camera is off the ground
+//    * @param cameraPitchRadians     The Pitch that the camera is at on the robot
+//    */
+//   public Camera(String cameraBroadcastingName, Transform3d cameraTransform) {
+//     camera = new PhotonCamera(cameraBroadcastingName);
+//     transform = cameraTransform;
+//   }
+  
+//   /**
+//    * @deprecated Doesn't work :(
+//    * @return The distance from the robot camera to the target given that it's a tag; -1 if there is no tag
+//    */
+//   public double getDistanceMeters() {
+//     if (!isResultUsable()) { return -1; }
+
+//     double tagAngle = Units.degreesToRadians(getPitch());
+//     double tagHeight = transform.getZ();
+
+//     double distance = PhotonUtils.calculateDistanceToTargetMeters(
+//      transform.getTranslation().getZ(),
+//      tagHeight, 
+//      transform.getRotation().getX(), 
+//      tagAngle
+//     );
+//     return (distance);
+//   }
+  
+//   /** @return The pitch of the current best target */
+//   public double getPitch() {
+//     return isResultUsable() ? result.getBestTarget().getPitch() : -1;
+//   }
+//   /** @return The Area that the current best target takes up */
+//   public double getArea() {
+//     return isResultUsable() ? result.getBestTarget().getArea() : -1;
+//   }
+//   /** @return The skew of the current best target */
+//   public double getSkew() {
+//     return isResultUsable() ? result.getBestTarget().getSkew() : -1;
+//   }
+//   /** @return The yaw of the current best target; -1 if there are no targets */
+//   public double getYaw() {
+//     return isResultUsable() ? result.getBestTarget().getYaw() : -1;
+//   }
+
+//   /**
+//    * Updates the camera with all the results
+//    * Should be called after each cycle (loop of higher function)
+//    * Aka: Should be called before using data (but not while parsing data)
+//    */
+//   public void update() { 
+//     // var results = camera.getAllUnreadResults().toArray(new PhotonPipelineResult[0]);
+//     // result = results.length == 0 ? null : results[results.length-1];
+//     result = camera.getLatestResult();
+//   }
+
+//   /**
+//    * @return True if the camera can be used to get data
+//    */
+//   public boolean isResultUsable() {
+//     // if the camera was updated
+//     // and if the camera see's something
+//     if(result == null || !result.hasTargets()) {
+//       try{
+//         System.out.println(!result.hasTargets() ? "\nCamera could not see any targets" : "");
+//       } catch(Exception e) { //Can't access result if it's null, so it throws an error.
+//         System.out.println("\nCamera does not have a result. Likely updated too many times");
+//       } 
+//       return false;
+//     } else return true;
+//   }
+
+//   /** Takes a screenshot can be found at the bottom of the settings menu (10.91.49.11) */
+//   public void screenshot() { camera.takeInputSnapshot(); System.out.println("You took a screenshot!"); }
+// }
+
--- src/main/java/com/robocats/vision/LimelightCamera.java
+++ src/main/java/com/robocats/vision/LimelightCamera.java
@@ -7,8 +7,10 @@ import com.robocats.vision.LimelightHelpers.RawFiducial;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.PoseEstimator;
-import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
@@ -17,13 +19,17 @@ public class LimelightCamera {
     private double cameraHeight;
     private double cameraAngle;
     private AprilTagFieldLayout tagLayout;
+    //This is the estimate of the position
+    //We need to get a poseEstimator
+    //We still need PoseEstimate though
     private PoseEstimate positionEstimation;
+    private PoseEstimator PLACEHOLDER;
 
     public LimelightCamera(double cameraHeight, double cameraAngle, AprilTagFields tagLayout, String cameraName) {
         this.cameraHeight = cameraHeight;
         this.cameraAngle = cameraAngle;
         this.tagLayout = AprilTagFieldLayout.loadField(tagLayout);
-        
+
         Optional<Alliance> ally = DriverStation.getAlliance();
         // if we are not on a team or on blue team, use the blue pose estimate
         // if we are on a team and not blue team, use red.
@@ -34,7 +40,7 @@ public class LimelightCamera {
         // LimelightHelpers.SetRobotOrientation(cameraName, robotAngle, 0, 0, 0, 0, 0);
     }
 
-    public Pose3d getRobotPose() {
+    public Pose2d getRobotPose() {
         RawFiducial[] fiducials = LimelightHelpers.getRawFiducials("");
         for (RawFiducial fiducial : fiducials) {
             int id = fiducial.id; // Tag ID
