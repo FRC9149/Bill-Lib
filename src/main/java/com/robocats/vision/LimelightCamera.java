c6153afae10f661e774a3570902de086a42cbec1
= <=>
2026-01-22 19:29:24 -0600

    updated limelight camera

--- src/main/java/com/robocats/vision/LimelightCamera.java
+++ src/main/java/com/robocats/vision/LimelightCamera.java
@@ -1,15 +1,53 @@
 package com.robocats.vision;
 
+import java.util.Optional;
+
+import com.robocats.vision.LimelightHelpers.PoseEstimate;
+import com.robocats.vision.LimelightHelpers.RawFiducial;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.estimator.PoseEstimator;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
 public class LimelightCamera {
-    // Basic targeting data
-    public void tutorial() {
-        double tx = LimelightHelpers.getTX("");  // Horizontal offset from crosshair to target in degrees
-        double ty = LimelightHelpers.getTY("");  // Vertical offset from crosshair to target in degrees
-        double ta = LimelightHelpers.getTA("");  // Target area (0% to 100% of image)
-        boolean hasTarget = LimelightHelpers.getTV(""); // Do you have a valid target?
-        
-        double txnc = LimelightHelpers.getTXNC("");  // Horizontal offset from principal pixel/point to target in degrees
-        double tync = LimelightHelpers.getTYNC("");  // Vertical  offset from principal pixel/point to target in degrees
-        LimelightHelpers.setPipelineIndex("", 0);
+    // Basic targeting data+
+    private double cameraHeight;
+    private double cameraAngle;
+    private AprilTagFieldLayout tagLayout;
+    private PoseEstimate positionEstimation;
+
+    public LimelightCamera(double cameraHeight, double cameraAngle, AprilTagFields tagLayout, String cameraName) {
+        this.cameraHeight = cameraHeight;
+        this.cameraAngle = cameraAngle;
+        this.tagLayout = AprilTagFieldLayout.loadField(tagLayout);
+        Optional<Alliance> ally = DriverStation.getAlliance();
+        if (ally.isPresent()) {
+            if (ally.get() == Alliance.Red) {
+                this.positionEstimation = LimelightHelpers.getBotPoseEstimate_wpiRed_MegaTag2(cameraName);
+            }
+            if (ally.get() == Alliance.Blue) {
+                this.positionEstimation = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(cameraName);
+            }
+        }
+        else {
+            this.positionEstimation = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(cameraName);
+        }
+    }
+
+    public Pose3d getRobotPose() {
+        RawFiducial[] fiducials = LimelightHelpers.getRawFiducials("");
+        for (RawFiducial fiducial : fiducials) {
+            int id = fiducial.id; // Tag ID
+            double txnc = fiducial.txnc; // X offset (no crosshair)
+            // double tync = fiducial.tync; // Y offset (no crosshair)
+            double ta = fiducial.ta; // Target area
+            double distToCamera = fiducial.distToCamera; // Distance to camera
+            double distToRobot = fiducial.distToRobot; // Distance to robot
+            double ambiguity = fiducial.ambiguity; // Tag pose ambiguity
+        }
+        return null;
     }
 }
