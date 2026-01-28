8e55cfecb8ff05dea8d140f745829cc0f7a8c533
SomeTrees266 <4estgx@gmail.com>
2026-01-28 17:08:00 -0600

    Hugo proof Null for Bill the farmer

---
+++ src/main/java/com/robocats/YallKnowWhatThisIs/Null.java
@@ -0,0 +1,19 @@
+package com.robocats.YallKnowWhatThisIs;
+
+public class Null {
+    private final double NULL = 0;
+    public final double nulL = NULL;
+    public final double nuLl = NULL;
+    public final double nuLL = NULL;
+    public final double nUll = NULL;
+    public final double nUlL = NULL;
+    public final double nULl = NULL;
+    public final double nULL = NULL;
+    public final double Null = NULL;
+    public final double NulL = NULL;
+    public final double NuLl = NULL;
+    public final double NuLL = NULL;
+    public final double NUll = NULL;
+    public final double NUlL = NULL;
+    public final double NULl = NULL;
+}
