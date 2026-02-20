d2ae2055196f365682b9815bb685d133da13f469
SomeTrees266 <4estgx@gmail.com>
2026-02-20 07:30:25 -0600

    Lights

--- src/main/java/com/robocats/LED/LedStrip.java
+++ src/main/java/com/robocats/LED/LedStrip.java
@@ -1,24 +1,53 @@
 package com.robocats.LED;
 
+import java.util.Map;
 import java.util.function.BiConsumer;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class LedStrip {
+/**
+    //TODO
+    Once we are done with writing this class, we should move it to the library.
+    I moved the class to the main project so that it is easier to write code. 
+    The implementation will be the same, just different imports
+
+    LEDPatterns seem to be the easiet way to program complex patterns while simple or smaller patterns will be used with setLED
+*/
+public class LedStrip extends SubsystemBase {
     private AddressableLED led;
     private AddressableLEDBuffer ledBuffer;
+
 
     /**
      * @param port The pwm port that the LED Strip is connected to
      * @param length How many LEDs are on the strip
      */
+
+    //------------------------base stuff--------------------------------------
     public LedStrip(int port, int length) {
         led = new AddressableLED(port);
         ledBuffer = new AddressableLEDBuffer(length);
         led.setLength(length);
+
+        led.start();
     }
+
+     public void applyActiveLEDPattern(LEDPattern newPattern) {
+
+    }
+
+    //@Override
+    //public void periodic() {
+    //    // Runs every 20ms to update the animation frames
+    //    activePattern.applyTo(ledBuffer);
+    //    led.setData(ledBuffer);
+    //}
+    
+    //------------------------------------more stuff------------------------------------------
 
     /** Sets a led to a certain color
      * @param index The index of the led you want to change
@@ -60,9 +89,16 @@ public class LedStrip {
     /** stops output to the led strip*/
     public void stop() { led.stop();}
 
-    public void setAll(int r, int g, int b) {}
+    public void setAll(int r, int g, int b) {
+
+        for (int index = 0; index < ledBuffer.getLength(); index++) {
+            ledBuffer.setRGB(index, r, g, b);
+
+        }
+        led.setData(ledBuffer);
+    }
     public void setRainbow() {}
-    // TODO find a better name for this
+    // todo find a better name for this
     // should make a line that moves up the led strip
     //ex:
     // _____====__________
@@ -70,4 +106,18 @@ public class LedStrip {
     // _____________====__
     // ...
     public void setLoading() {}
+
+    public void sethalf(){
+
+        LEDPattern steps = LEDPattern.steps(Map.of(0, Color.kWhite, 0.5, Color.kBlue));
+
+// Apply the LED pattern to the data buffer
+steps.applyTo(ledBuffer);
+
+// Write the data to the LED strip
+led.setData(ledBuffer);
+      
+
+
+    }
 }
