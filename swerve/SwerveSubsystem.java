142200bef1e9662531a965f5b8d890c8c74c8e0b
SomeTrees266 <4estgx@gmail.com>
2026-03-03 20:36:28 -0600

    pthplnr

--- swerve/SwerveSubsystem.java
+++ swerve/SwerveSubsystem.java
@@ -26,6 +26,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -306,10 +307,11 @@ His name is Jeremy...
 
         Command c = AutoBuilder.pathfindToPose(pose, constraints);
         c.addRequirements(this);
-        List<Pose2d> listPose = new List<Pose2d>();
-        listPose.add(pose);
-        PathPlannerPath path = PathPlannerPath.waypointsFromPoses(listPose);
-        return new FollowPathCommand(null, null, null, null, null, robotConfig, null, null)
+        // List<Pose2d> listPose = new List<Pose2d>();
+        // listPose.add(pose);
+        // PathPlannerPath path = PathPlannerPath.waypointsFromPoses(listPose);
+        return c;
+        // return new FollowPathCommand(path, this::getPose, null, null, null, robotConfig, null, null);
     }
 
     /**
