ecca17f6739b9d4d0778906bac09df7d04a91153
A Humongous Amount Of Trees <4estgx@gmail.com>
2026-03-04 02:12:30 +0000

    Added the field

--- swerve/SwerveSubsystem.java
+++ swerve/SwerveSubsystem.java
@@ -33,6 +33,7 @@ public class SwerveSubsystem extends SubsystemBase {
     private SwerveModule frontRight;
     private SwerveModule backRight;
     private AprilCamera camera;
+    private final Field2d m_field;
     // https://docs.wpilib.org/en/stable/docs/software/kinematics-and-odometry/swerve-drive-odometry.html
     SwerveDriveOdometry odometry;
 
@@ -46,9 +47,10 @@ public class SwerveSubsystem extends SubsystemBase {
      *                      turning motors
      * @param setupPathPlanner Defines if pathplanner should be setup automatically. Otherwise you can call `setupPathPlanner();` yourself
      */
-    public SwerveSubsystem(SwerveConfig config, PIDController test, boolean setupPathPlanner) {
+    public SwerveSubsystem(SwerveConfig config, PIDController test, boolean setupPathPlanner, Field2d field) {
         swerveConfig = config;
         turnController = test;
+        this.m_field = field;
         
         turnController.enableContinuousInput(0, 2 * Math.PI);
 
@@ -166,6 +168,7 @@ His name is Jeremy...
         if(camera != null) {
             camera.periodic();
         }
+        m_field.setRobotPose(m_odometry.getPoseMeters());
     }
    
     public Pose2d getPose() {
