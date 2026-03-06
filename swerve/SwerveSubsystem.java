9874e07cd62ff74036a7fcf8699b8cfbbbf8cfb9
El <105391840+El-404@users.noreply.github.com>
2026-03-06 15:00:27 -0600

    Update comments in SwerveSubsystem.java
    Commented out code and added notes for future reference.

--- swerve/SwerveSubsystem.java
+++ swerve/SwerveSubsystem.java
@@ -469,6 +469,8 @@ His name is Jeremy...
 
                 //The code below is just another, seemingly smoother way to write it, but it 
                 //didn't do anything at all to change the controls when I tested it.
+
+                //That's because it does the same thing... You said that yourself
             
                 //----o        ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooouu-------WE SHOULD LOOK INTO THIS------------------,_I_THink_it_may_work_with_some_adjustment,_not_my_code
                 //// Reference to this subsystem to set requirements
