8d5a35e3a1fde99f0509fc2e469d84816dc44eb3
El <ecampus2026@gmail.com>
2026-04-13 15:00:28 -0500

    cleaned LedCode - not done - it's bad

--- LED/LedStrip.java
+++ LED/LedStrip.java
@@ -1,5 +1,6 @@
 package com.robocats.LED;
 
+import java.sql.Driver;
 import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.BooleanSupplier;
@@ -13,31 +14,24 @@ import static edu.wpi.first.units.Units.Second;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-/**
-    //TODO
-    Once we are done with writing this class, we should move it to the library.
-    I moved the class to the main project so that it is easier to write code. 
-    The implementation will be the same, just different imports
 
-    LEDPatterns seem to be the easiet way to program complex patterns while simple or smaller patterns will be used with setLED
-*/
+
 public class LedStrip extends SubsystemBase {
     private AddressableLED led;
     private AddressableLEDBuffer ledBuffer;
-    private double globalBrightness = 50; // 0 to 100 as a percentage
+    private double globalBrightness = .5; // 0 to 1 as a percentage
 
 
     /**
      * @param port The pwm port that the LED Strip is connected to
      * @param length How many LEDs are on the strip
      */
-
-    //------------------------base stuff--------------------------------------
     public LedStrip(int port, int length) {
         led = new AddressableLED(port);
         ledBuffer = new AddressableLEDBuffer(length);
@@ -45,21 +39,6 @@ public class LedStrip extends SubsystemBase {
 
         led.start();
     }
-
-     public void applyActiveLEDPattern(LEDPattern newPattern) {
-
-
-
-    }
-
-    //@Override
-    //public void periodic() {
-    //    // Runs every 20ms to update the animation frames
-    //    activePattern.applyTo(ledBuffer);
-    //    led.setData(ledBuffer);
-    //}
-    
-    //------------------------------------more stuff------------------------------------------
 
     /** Sets a led to a certain color
      * @param index The index of the led you want to change
@@ -72,86 +51,44 @@ public class LedStrip extends SubsystemBase {
         led.setData(ledBuffer);
     }
 
+    /**
+     * Changes the brightness of all LedPatterns
+     * @param brightness The percent brightness from 0 to 1, where 0 is off and 1 is full brightness
+     */
     public void setGlobalBrightness(double brightness) {
-    globalBrightness = MathUtil.clamp(brightness, 0.0, 1.0);
+        globalBrightness = MathUtil.clamp(brightness, 0.0, 1.0);
     }
 
-    public Color COLOR(int rOne, int gOne, int bOne){
-        Color color = new Color(rOne, gOne, bOne);
+    /**
+     * Turns rgb values into a Color object that is usable by LedPatterns
+    */
+    private Color rgbToColor(int r, int g, int b){
+        Color color = new Color(r, g, b);
         return color;
     }
 
-    public Color[] COLOR(int rOne, int gOne, int bOne, int rTwo, int gTwo, int bTwo) {
-        Color color_one = new Color(rOne, gOne, bOne);
-        Color color_two = new Color(rTwo, gTwo, bTwo);
-        Color[] color_array = {color_one, color_two};
-        return color_array;
-    }
 
-    public Color[] COLOR(int rOne, int gOne, int bOne, int rTwo, int gTwo, int bTwo, int rThree, int gThree, int bThree) {
-        Color color_one = new Color(rOne, gOne, bOne);
-        Color color_two = new Color(rTwo, gTwo, bTwo);
-        Color color_three = new Color(rThree, gThree, bThree);
-        Color[] color_array = {color_one, color_two, color_three};
-        return color_array;
-    }
+    /**
+     * Sets the leds that have in index equal to a number in the fibonacci sequence to a certain color
+     */
+    public void fibonacciSequence(int r, int g, int b) {
+        setAll(0, 0, 0);
+        int a = 0;
+        int c = 1;
+        
+        while(true) {
+            int next = a + c;
+            a = c;
+            c = next;
 
-    public Color[] COLOR(int rOne, int gOne, int bOne, int rTwo, int gTwo, int bTwo, int rThree, int gThree, int bThree, int rFour, int gFour, int bFour) {
-        Color color_one = new Color(rOne, gOne, bOne);
-        Color color_two = new Color(rTwo, gTwo, bTwo);
-        Color color_three = new Color(rThree, gThree, bThree);
-        Color color_four = new Color(rFour, gFour, bFour);
-        Color[] color_array = {color_one, color_two, color_three, color_four};
-        return color_array;
-    }
-
-        public Color[] COLOR(int rOne, int gOne, int bOne, int rTwo, int gTwo, int bTwo, int rThree, int gThree, int bThree, int rFour, int gFour, int bFour, int rFive, int gFive, int bFive) {
-        Color color_one = new Color(rOne, gOne, bOne);
-        Color color_two = new Color(rTwo, gTwo, bTwo);
-        Color color_three = new Color(rThree, gThree, bThree);
-        Color color_four = new Color(rFour, gFour, bFour);
-        Color color_five = new Color(rFive, gFive, bFive);
-        Color[] color_array = {color_one, color_two, color_three, color_four, color_five};
-        return color_array;
-    }
-
-    
-
-
-    public void LED_fibonacci_sequence(int r, int g, int b) {
-
-        int i;
-        for (i = 0; i <= ledBuffer.getLength(); i++) {
-            
-            int the_index;
-            int a = 0;
-            int c = 1;
-
-            if (i == 0) {
-                the_index = 0;
-            }
-            
-            for (int n = 2; n <= i; n++) {
-                int next = a + c;
-                a = c;
-                c = next;
-            }
-
-            the_index = c;
-
-            if (the_index <= ledBuffer.getLength()){
-            setLed(the_index, r, g, b);
+            if (c <= ledBuffer.getLength()){
+                setLed(c, r, g, b);
+            } else {
+                return;
             }
         }
 
     }
-
-
-
-    public void two_color_mask() {
-
-    }
-
 
 
     /** Takes in a BiConsumer that gets applied to all the led pixels.
@@ -177,156 +114,85 @@ public class LedStrip extends SubsystemBase {
      * @param pattern The led pattern you wish to apply
      */
     public void applyLEDPattern(LEDPattern pattern) {
+        applyBrightness(pattern);
         pattern.applyTo(ledBuffer);
         led.setData(ledBuffer);
     }
-    //// EX This makes an LED pattern that scrolls a gradient at 10.5 percent of the strip per second
-    //subsystem.applyLEDPattern(
-    //    subsystem.scroll(
-    //    subsystem.make_gradient(
-    //        subsystem.COLOR(237, 100, 195)
-    //    ), 
-    //    10.5
-    //    )
-    //);
 
-    //Not for use outside of here I think
     private LEDPattern applyBrightness(LEDPattern pattern) {
-
-        LEDPattern pattern_with_brightness = pattern.atBrightness(Percent.of(globalBrightness));
-
-        return pattern_with_brightness;
-
-
+        return pattern.atBrightness(Percent.of(globalBrightness * 100));
     }
 
     /** stops output to the led strip*/
     public void stop() { led.stop();}
 
+    /**
+     * sets all the leds to a certain color
+     */
     public void setAll(int r, int g, int b) {
-
         for (int index = 0; index < ledBuffer.getLength(); index++) {
             ledBuffer.setRGB(index, r, g, b);
-
         }
         led.setData(ledBuffer);
     }
 
-    public LEDPattern reverse(LEDPattern base) {
-        LEDPattern pattern = base.reversed();
-        
+    public LEDPattern progressMask(LEDPattern base, DoubleSupplier percentage) {
+        LEDPattern mask = LEDPattern.progressMaskLayer(percentage);
+        return base.mask(mask);
+    }
+
+    public LEDPattern gradient(Color... colors){
+        LEDPattern pattern = LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, colors); 
         return applyBrightness(pattern);
     }
 
-    public LEDPattern scroll(LEDPattern base, double percentage_of_strip) {
-        LEDPattern pattern = base.scrollAtRelativeSpeed(Percent.per(Second).of(percentage_of_strip));
-        return applyBrightness(pattern);
-    }
-
-    public LEDPattern progress_mask(LEDPattern base, DoubleSupplier percentage) {
-    LEDPattern mask = LEDPattern.progressMaskLayer(percentage);
-    LEDPattern pattern = base.mask(mask);
-    return applyBrightness(pattern);
-    }
-
-    public void setRainbow() {}
-    // todo find a better name for this
-    // should make a line that moves up the led strip
-    //ex:
-    // _____====__________
-    // _________====______
-    // _____________====__
-    // ...
-//MUST BE PUT INTO A PERIODIC FUCTION OR SOMETHING IDK MAN 
-
-//YEAH, I MADE IT ONE LINE AS A JOKE, BUT LEFT IT THAT WAY BECAUSE IT WAS FUNNIER, LMAO
-//It used to be 4 lines, I saved 3 WHOLE lines that I WILL be WASTING somewhere else
-// 5 lines now, saved 4 lines*
-// 4 lines now, saved 3 lines*
-    public LEDPattern make_gradient(Color... colors){LEDPattern pattern = LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, colors); return applyBrightness(pattern);}
-
-    //SYMMETRICAL:
-    public LEDPattern make_blinking_gradient(double breathingTime, Color... colors){
-   
-        // Create an LED pattern that displays a red-to-blue gradient, breathing at a 2 second period (0.5 Hz)
+    public LEDPattern blinkingGradient(double breathingTime, Color... colors){
         LEDPattern base = LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, colors);
-        LEDPattern pattern =  base.blink(Seconds.of(breathingTime));
-        return applyBrightness(pattern);
+        return base.blink(Seconds.of(breathingTime));
     }
 
-    public LEDPattern blinking(double blinkingTime, LEDPattern base) {
-
-        LEDPattern pattern =  base.blink(Seconds.of(blinkingTime));
-
-        return applyBrightness(pattern);
-
+    public LEDPattern blinking(LEDPattern base, double blinkingTime) {
+        return base.blink(Seconds.of(blinkingTime));
     }
 
-    //ASYMMETRICAL:
-    public LEDPattern make_blinking_gradient(double ON, double OFF, Color... colors){
-        // Create an LED pattern that displays a red-to-blue gradient, breathing at a 2 second period (0.5 Hz)
+    public LEDPattern blinkingGradient(double onTime, double offTime, Color... colors){
         LEDPattern base = LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, colors);
-        LEDPattern pattern =  base.blink(Seconds.of(ON), Seconds.of(OFF));
-
-        return applyBrightness(pattern);
-
-
+        return base.blink(Seconds.of(onTime), Seconds.of(offTime));
     }
 
-    public LEDPattern blinking(double ON, double OFF, LEDPattern base) {
-
-        LEDPattern pattern =  base.blink(Seconds.of(ON), Seconds.of(OFF));
-        
-        return applyBrightness(pattern);
-
-    }
-
-    //FOR SYNCHONIZATION:
-    public LEDPattern make_blinking_gradient(BooleanSupplier THE_TRUTH, Color... colors){
-        // Create an LED pattern that displays a red-to-blue gradient, breathing at a 2 second period (0.5 Hz)
+    /**
+     * @param signal When this supplier returns true, the gradient will be on, and off when false
+     * @param colors a set of colors that will be used in the gradient
+     * @return a pattern that will blink the given gradient based on the signal
+     */
+    public LEDPattern blinkingGradient(BooleanSupplier signal, Color... colors){
         LEDPattern base = LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, colors);
-        //CAN BE USED WITH SOMETHING LIKE: synchronizedBlink(RobotController::getRSLState) to sync with the RSL
-        LEDPattern pattern =  base.synchronizedBlink(THE_TRUTH);
-
-        return applyBrightness(pattern);
-
+        return base.synchronizedBlink(signal);
     }
 
-    public LEDPattern blinking(BooleanSupplier THE_TRUTH, LEDPattern base) {
-
-        LEDPattern pattern =  base.synchronizedBlink(THE_TRUTH);
-        
-        return applyBrightness(pattern);
-
-    }
-
-    //Pretty self explanetory
-    public LEDPattern make_breathing_gradient(double breathingTime, Color... colors){
-
-    // Create an LED pattern that displays a red-to-blue gradient, breathing at a 2 second period (0.5 Hz)
+    public LEDPattern breathingGradient(double breathingTime, Color... colors){
         LEDPattern base = LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, colors);
-        LEDPattern pattern =  base.breathe(Seconds.of(breathingTime));
-
-        return applyBrightness(pattern);
-
+        return base.breathe(Seconds.of(breathingTime));
     }
-
-    public LEDPattern breathing(double breathingTime, LEDPattern base) {
-
-        LEDPattern pattern =  base.breathe(Seconds.of(breathingTime));
-
-        return applyBrightness(pattern);
-
-    }
-
-
-
-    public void setLoading() {}
 
     public LEDPattern sethalf(Color color_one, Color color_two){
+        return LEDPattern.steps(Map.of(0, color_one, 0.5, color_two));
+    }
 
-        LEDPattern pattern = LEDPattern.steps(Map.of(0, color_one, 0.5, color_two));
-        return applyBrightness(pattern);
+    public void setAllianeColor(Color defaultColor) {
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+            switch(alliance.get()) {
+                case Red:
+                    setAll(255, 0, 0);
+                    break;
+                case Blue:
+                    setAll(0, 0, 255);
+                    break;
+                default:
+                    setAll((int)(defaultColor.red * 255), (int)(defaultColor.green * 255), (int)(defaultColor.blue * 255));
+            }
+        }
     }
 
     
@@ -357,22 +223,24 @@ public class LedStrip extends SubsystemBase {
 */
 
 
-    public void Prettycolors() {
-        LEDPattern pattern = blinking(3.78654321, scroll(make_breathing_gradient(9.0, COLOR(127,255,0, 75, 83, 32)), 11.786));
-        applyLEDPattern(pattern);
-
-    }
-    public void Flash() {
-        LEDPattern pattern = make_blinking_gradient(.1, COLOR(255,255,255));
+    public void prettyColors() {
+        LEDPattern pattern = blinking(
+            breathingGradient(9.0, rgbToColor(127,255,0), rgbToColor(75, 83, 32)).scrollAtRelativeSpeed(Percent.per(Second).of(11.786)), 
+            3.78654321
+        );
         applyLEDPattern(pattern);
     }
-    public void BoringSolidColorsNavy() {
+    public void flash() {
+        LEDPattern pattern = blinkingGradient(.1, rgbToColor(255,255,255));
+        applyLEDPattern(pattern);
+    }
+    public void solidNavy() {
         setAll(0,0,128);
     }
-    public void BoringSolidColorsVermilion() {
+    public void solidVermilion() {
         setAll(227,66,52);
     }
-    public void BoringSolidColorsIvyGreen() {
+    public void solidIvyGreen() {
         setAll(0,106,91);
     }
 
